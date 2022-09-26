0b629858dcd423ca4f5b68c6daa6a810b539f21d
John Climie <johnclimie00@gmail.com>
2022-09-26 12:49:41 -0400

    Created unsellectAll function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('test');
+let colors = document.querySelectorAll('.color');
+
+let i;
+
+function unselectAll() {
+    for (i = 0; i < colors.length; i++) {
+        colors[i].style.backgroundColor = "";
+    }
+}
