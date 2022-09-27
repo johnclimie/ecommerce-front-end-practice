77710f4ed73f554295eb0914c7f20f7598010e38
John Climie <johnclimie00@gmail.com>
2022-09-27 12:15:50 -0400

    Added comments two script file

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// Create variable for all elements with color class
 let colors = document.querySelectorAll('.color');
 
+// Function to unselect the elements when an element is clicked
 function unselectAll() {
     for (let i = 0; i < colors.length; i++) {
         colors[i].style.border = "";
     }
 }
 
-
+// Adds an event listener to each color element
 colors.forEach(color => {
     color.addEventListener('click', function() {
         unselectAll();
