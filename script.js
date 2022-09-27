6196b3ca350340df35ea0769ef0bfd4b83ed61d5
John Climie <johnclimie00@gmail.com>
2022-09-27 13:26:33 -0400

    Changed unselectAll function to include a parameter

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 let colors = document.querySelectorAll('.color');
 
 // Function to unselect the elements when an element is clicked
-function unselectAll() {
-    for (let i = 0; i < colors.length; i++) {
-        colors[i].style.border = "";
+function unselectAll(cls) {
+    for (let i = 0; i < cls.length; i++) {
+        cls[i].style.border = "";
     }
 }
 
 // Adds an event listener to each color element
 colors.forEach(color => {
     color.addEventListener('click', function() {
-        unselectAll();
+        unselectAll(colors);
         this.style.border = "solid black 1px";
     });
 });
